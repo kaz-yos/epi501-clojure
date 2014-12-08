eee0b48b247b17823047c63d6c646fd94858f870
kaz-yos <kazky@mac.com>
2014-12-07 21:59:12 -0500

    Add minimum data representation and query functions

--- src/epi501/core.clj
+++ src/epi501/core.clj
@@ -3,42 +3,43 @@
 
 
 ;;; Data representation
-(def node1 {:id 1, :neighbors [1 2 3]})
-(:id node1)
-(:neighbors node1)
-
-(def node2 {:id 2, :neighbors [1]})
-(def node3 {:id 3, :neighbors [1 4]})
-(def node4 {:id 4, :neighbors [3]})
-(def pop1 [node1 node2 node3 node4])
+;; ;; A node is a map with an id and a neighbors vector
+;; (def node1 {:id 1, :neighbors [2 3]})
+;; (def node2 {:id 2, :neighbors [1]})
+;; (def node3 {:id 3, :neighbors [1 4]})
+;; (def node4 {:id 4, :neighbors [3]})
+;; (def node5 {:id 5, :neighbors []})
+;; ;; A population is a vector of nodes
+;; (def pop1 [node1 node2 node3 node4 node5])
 
 
 ;;; Query functions
 
-;; Function to obtain edges
+;; Function to obtain edges from a node
+;; map -> seq seq
 (defn edges [node]
-  
   (let [ego-id    (:id node)
         alter-ids (:neighbors node)
-        edge (fn [alter-id]
-               [ego-id alter-id])]
+        edge      (fn [alter-id]
+                    [ego-id alter-id])]
     (cond
-     (empty? alter-ids) []
+     (empty? alter-ids) '() ; empty seq of seqs
      :else              (map edge alter-ids))))
 
-(edges node1)
-(map edges pop1)
-
-(->> (map edges pop1)
-     (reduce concat)
-     (map sort)
-     (set))
-
-;; function to extract unique edges from a population
+;; function to extract unique undirected edges from a population
 ;; map vector -> seq set
-(defn unique-edges [pop]
+(defn unique-undirected-edge-set [pop]
   (->> (map edges pop)
-       ))
+       (reduce concat)
+       (map sort)
+       (set)))
+
+;; function to extract unique directed edges from a population
+;; map vector -> seq set
+(defn unique-directed-edge-set [pop]
+  (->> (map edges pop)
+       (reduce concat)
+       (set)))
 
 ;;; Main function for entry
 (defn -main
--- test/epi501/core_test.clj
+++ test/epi501/core_test.clj
@@ -2,6 +2,49 @@
   (:require [clojure.test :refer :all]
             [epi501.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+;;; Data representation and definition
+;; A node is a map with an id and a neighbors vector
+(def node1 {:id 1, :neighbors [2 3]})
+(def node2 {:id 2, :neighbors [1]})
+(def node3 {:id 3, :neighbors [1 4]})
+(def node4 {:id 4, :neighbors [3]})
+(def node5 {:id 5, :neighbors []})
+;; A population is a vector of nodes
+(def pop1 [node1 node2 node3 node4 node5])
+
+
+;;; Node-level information extraction 
+(deftest id-test
+  (testing "id extration"
+    (is (= (:id node1) 1))
+    (is (= (:id node2) 2))
+    (is (= (:id node3) 3))
+    (is (= (:id node4) 4))
+    (is (= (:id node5) 5))))
+
+(deftest neighbors-test
+  (testing "id extration"
+    (is (= (:neighbors node1) [2 3]))
+    (is (= (:neighbors node2) [1]))
+    (is (= (:neighbors node3) [1 4]))
+    (is (= (:neighbors node4) [3]))
+    (is (= (:neighbors node5) []))))
+
+(deftest edges-test
+  (testing "Extraction of edges from a node"
+    (is (= (edges node1) [[1 2] [1 3]]))
+    (is (= (edges node2) [[2 1]]))
+    (is (= (edges node3) [[3 1] [3 4]]))
+    (is (= (edges node4) [[4 3]]))
+    (is (= (edges node5) []))))
+
+
+;;; Population-level information extraction
+(deftest unique-undirected-edge-set-test
+  (testing "Extraction of edge set"
+    (is (= (unique-undirected-edge-set pop1) #{[1 2] [1 3] [3 4]}))))
+
+(deftest unique-directed-edge-set-test
+  (testing "Extraction of edge set"
+    (is (= (unique-directed-edge-set pop1) #{[1 2] [1 3] [2 1] [3 1] [3 4] [4 3]}))))
+
