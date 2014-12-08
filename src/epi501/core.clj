00ec0a03ad26a2a76019f0481096f8effe08bb80
kaz-yos <kazky@mac.com>
2014-12-07 22:10:59 -0500

    Add :state and :time to data representation

--- src/epi501/core.clj
+++ src/epi501/core.clj
@@ -2,19 +2,12 @@
   (:gen-class))
 
 
+;;; 
 ;;; Data representation
-;; ;; A node is a map with an id and a neighbors vector
-;; (def node1 {:id 1, :neighbors [2 3]})
-;; (def node2 {:id 2, :neighbors [1]})
-;; (def node3 {:id 3, :neighbors [1 4]})
-;; (def node4 {:id 4, :neighbors [3]})
-;; (def node5 {:id 5, :neighbors []})
-;; ;; A population is a vector of nodes
-;; (def pop1 [node1 node2 node3 node4 node5])
+;; See the test file
 
-
+;;; 
 ;;; Query functions
-
 ;; Function to obtain edges from a node
 ;; map -> seq seq
 (defn edges [node]
@@ -41,6 +34,7 @@
        (reduce concat)
        (set)))
 
+;;; 
 ;;; Main function for entry
 (defn -main
   "I don't do a whole lot ... yet."
--- test/epi501/core_test.clj
+++ test/epi501/core_test.clj
@@ -4,11 +4,11 @@
 
 ;;; Data representation and definition
 ;; A node is a map with an id and a neighbors vector
-(def node1 {:id 1, :neighbors [2 3]})
-(def node2 {:id 2, :neighbors [1]})
-(def node3 {:id 3, :neighbors [1 4]})
-(def node4 {:id 4, :neighbors [3]})
-(def node5 {:id 5, :neighbors []})
+(def node1 {:id 1, :neighbors [2 3], :state :I,  :time 2})
+(def node2 {:id 2, :neighbors [1],   :state :R,  :time 1})
+(def node3 {:id 3, :neighbors [1 4], :state :E,  :time 1})
+(def node4 {:id 4, :neighbors [3],   :state :S,  :time 2})
+(def node5 {:id 5, :neighbors [],    :state :D2, :time 3})
 ;; A population is a vector of nodes
 (def pop1 [node1 node2 node3 node4 node5])
 
@@ -29,6 +29,22 @@
     (is (= (:neighbors node3) [1 4]))
     (is (= (:neighbors node4) [3]))
     (is (= (:neighbors node5) []))))
+
+(deftest state-test
+  (testing "state extration"
+    (is (= (:state node1) :I))
+    (is (= (:state node2) :R))
+    (is (= (:state node3) :E))
+    (is (= (:state node4) :S))
+    (is (= (:state node5) :D2))))
+
+(deftest time-test
+  (testing "time extration"
+    (is (= (:time node1) 2))
+    (is (= (:time node2) 1))
+    (is (= (:time node3) 1))
+    (is (= (:time node4) 2))
+    (is (= (:time node5) 3))))
 
 (deftest edges-test
   (testing "Extraction of edges from a node"
