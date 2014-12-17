904c118a6c41061ced693292a576df54e5a49da2
kaz-yos <kazky@mac.com>
2014-12-16 21:56:43 -0500

    Change -main function for default plotting

--- src/epi501/core.clj
+++ src/epi501/core.clj
@@ -435,9 +435,7 @@
 
 ;; Transition from susceptible state
 ;; No automatic transition into E/I
-;;
 (def p-S->X {:S 1, :E 0, :I 0, :R 0})
-
 ;; Transition from exposed (infected & latent period)
 (def p-E->X {:E 6/7, :I 1/7, :S 0, :R 0})
 ;; Transition from infectious state
@@ -747,5 +745,30 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [test-graph1 (set-states (barabasi-albert-graph 5 1000 :undirectional 100) [89] :I)
+        ;; Transition from susceptible state
+        ;; No automatic transition into E/I
+        p-S->X {:S 1, :E 0, :I 0, :R 0}
+        ;; Transition from exposed (infected & latent period)
+        p-E->X {:E 6/7, :I 1/7, :S 0, :R 0}
+        ;; Transition from infectious state
+        p-I->X {:I 9/10, :R 1/10, :D1 1/2, :D2 1/2}
+        ;; Transition from hospitalized (also infectious) state
+        p-H->X {:H 9/10, :R 1/10, :D1 0, :D2 0}
+        ;; Recovered state is assumed to be an absorbing state
+        p-R->X {:R 1}
+        ;; Funeral/unsafe burial leads to safe burial over time
+        p-D1->X {:D2 1/2}
+        ;; Safe burial is assumed to be an absorbing state
+        p-D2->X {:D2 1}
+        p-A->X-map {:S p-S->X, :E p-E->X, :I p-I->X, :H p-H->X, :R p-R->X, :D1 p-D1->X, :D2 p-D2->X}
+        transmission-per-contact {:S 0, :E 0, :I 0.5, :H 0.5, :R 0, :D1 0.5, :D2 0}
+        maximum-n-of-contacts 5]
+    ;;
+    (->> (simulate p-A->X-map transmission-per-contact maximum-n-of-contacts test-graph1 100 20141216)
+      (graphs->compartments, )
+      (wide-dataset, )
+      (long-dataset, )
+      (line-chart, )
+      (view, ))))
 
